2ce1b2a8170ba8c0067d6937771fbb70a2faaa94
Rovshen Nazarov <rovshennazarov@gmail.com>
2020-11-04 14:53:27 -0600

    test

--- ex3/ex3-fixed.js
+++ ex3/ex3-fixed.js
@@ -39,6 +39,8 @@ NotesManager.prototype.hideHelp = function() {
 	this.$help.hide();
 };
 
+
+
 NotesManager.prototype.loadData = function(data) {
 	for (var i=0; i<data.length; i++) {
 		this.notes.push(data[i]);
